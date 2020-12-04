316c94d8afa24e07191b48a29e4877a20beaf71d
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-12-04 13:08:52 +0000

    Day 4 - part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,14 +1,77 @@
 import os
 import re
+from typing import Dict
+
+REQUIRED_FILES = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
 
-def solve_day4_part1(raw_input: str) -> int:
+def validate_byr(val: str) -> bool:
+    return len(val) == 4 and int(val) >= 1920 and int(val) <= 2002
+
+
+def validate_iyr(val: str) -> bool:
+    return len(val) == 4 and int(val) >= 2010 and int(val) <= 2020
+
+
+def validate_eyr(val: str) -> bool:
+    return len(val) == 4 and int(val) >= 2020 and int(val) <= 2030
+
+
+def validate_hgt(val: str) -> bool:
+    try:
+        height, unit = re.fullmatch(r"(\d+)(cm|in)", val).groups()
+    except AttributeError:
+        return False
+    height = int(height)
+    if unit == "cm":
+        return 150 <= height <= 193
+    if unit == "in":
+        return 59 <= height <= 76
+    return False
+
+
+def validate_hcl(val: str) -> bool:
+    return re.fullmatch(r"#[0-9a-f]{6}", val) is not None
+
+
+def validate_ecl(val: str) -> bool:
+    return val in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
+
+def validate_pid(val: str) -> bool:
+    return re.fullmatch(r"\d{9}", val) is not None
+
+
+def is_valid(passport: Dict, strict_validation: bool = False) -> bool:
+    if strict_validation is False:
+        return REQUIRED_FILES.issubset(passport.keys())
+
+    validation = {
+        "byr": validate_byr,
+        "iyr": validate_iyr,
+        "eyr": validate_eyr,
+        "hgt": validate_hgt,
+        "hcl": validate_hcl,
+        "ecl": validate_ecl,
+        "pid": validate_pid,
+    }
+
+    for field, check in validation.items():
+        if check(passport.get(field, "")) is False:
+            return False
+    return True
+
+
+def solve_day4_part1(raw_input: str, strict_validation: bool = False) -> int:
     raw_list_passports = raw_input.split(os.linesep * 2)
     re_passport_fields = re.compile(r"(\w+):([#]*\w+)")
-    required_fields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
     num_valid_passports = 0
     for passport in raw_list_passports:
         passport = dict(re_passport_fields.findall(passport))
-        if set(required_fields).issubset(passport.keys()):
+        if is_valid(passport, strict_validation):
             num_valid_passports += 1
     return num_valid_passports
+
+
+def solve_day4_part2(raw_input: str) -> int:
+    return solve_day4_part1(raw_input, strict_validation=True)
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -2,7 +2,17 @@ from pathlib import Path
 
 import pytest
 
-from day4.day4 import solve_day4_part1
+from day4.day4 import (
+    solve_day4_part1,
+    solve_day4_part2,
+    validate_byr,
+    validate_ecl,
+    validate_eyr,
+    validate_hcl,
+    validate_hgt,
+    validate_iyr,
+    validate_pid,
+)
 
 
 @pytest.fixture
@@ -33,5 +43,94 @@ def test_day4_part1_example(example_day4):
     assert solve_day4_part1(example_day4) == 2
 
 
-def test_day4_part1_input(input_day4):
-    assert solve_day4_part1(input_day4) == 196
+def test_day4_part2_invalid():
+    invalid = """eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007"""
+    assert solve_day4_part2(invalid) == 0
+
+
+def test_day4_part2_valid():
+    valid = """pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"""
+    assert solve_day4_part2(valid) == 4
+
+
+def test_validate_byr():
+    assert validate_byr("2000")
+    assert not validate_byr("89")
+    assert not validate_byr("1000")
+    assert not validate_byr("1919")
+    assert not validate_byr("2003")
+
+
+def test_validate_iyr():
+    assert validate_iyr("2011")
+    assert not validate_iyr("89")
+    assert not validate_iyr("1000")
+    assert not validate_iyr("2009")
+    assert not validate_iyr("2021")
+
+
+def test_validate_eyr():
+    assert validate_eyr("2025")
+    assert not validate_eyr("89")
+    assert not validate_eyr("2019")
+    assert not validate_eyr("2031")
+
+
+def test_validate_hgt():
+    assert validate_hgt("155cm")
+    assert validate_hgt("150cm")
+    assert validate_hgt("193cm")
+    assert validate_hgt("59in")
+    assert validate_hgt("76in")
+    assert validate_hgt("70in")
+
+    assert not validate_hgt("149cm")
+    assert not validate_hgt("194cm")
+    assert not validate_hgt("77in")
+    assert not validate_hgt("58in")
+
+    assert not validate_hgt("155mc")
+    assert not validate_hgt("a155")
+
+
+def test_validate_hcl():
+    assert validate_hcl("#123abc")
+    assert not validate_hcl("#123abz")
+    assert not validate_hcl("123abc")
+
+
+def test_validate_ecl():
+    assert validate_ecl("brn")
+    assert not validate_ecl("wat")
+
+
+def test_validate_pid():
+    assert validate_pid("000000001")
+    assert not validate_pid("0123456789")
+
+
+def test_day4_part2_input(input_day4):
+    assert solve_day4_part2(input_day4) == 114
