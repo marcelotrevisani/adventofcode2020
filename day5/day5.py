4adf7d8e87db6a11894e8562fa1451324e953ec9
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-12-06 22:46:30 +0000

    Day 4 - part 1

---
+++ day5/day5.py
@@ -0,0 +1,40 @@
+from enum import Enum, auto
+from typing import List
+
+
+class SeatPosition(Enum):
+    UPPER = auto()
+    LOWER = auto()
+
+
+def solve_day5_part1(
+    input_list: List[str],
+    *,
+    min_row: int = 0,
+    max_row: int = 127,
+    min_col: int = 0,
+    max_col: int = 7,
+) -> int:
+    max_id = 0
+    for line in input_list:
+        line = line.strip()
+        row = [SeatPosition.UPPER if n == "B" else SeatPosition.LOWER for n in line[:7]]
+        col = [SeatPosition.UPPER if n == "R" else SeatPosition.LOWER for n in line[7:]]
+        max_id = max(
+            max_id,
+            search_num(min_row, max_row, row) * 8 + search_num(min_col, max_col, col),
+        )
+    return max_id
+
+
+def search_num(lower_seat: int, upper_seat: int, seq: List[SeatPosition]) -> int:
+    if len(seq) == 1:
+        return lower_seat if seq[0] == SeatPosition.LOWER else upper_seat
+    if seq[0] == SeatPosition.UPPER:
+        return search_num(
+            lower_seat + ((upper_seat - lower_seat + 1) // 2), upper_seat, seq[1:]
+        )
+    elif seq[0] == SeatPosition.LOWER:
+        return search_num(
+            lower_seat, lower_seat + ((upper_seat - lower_seat) // 2), seq[1:]
+        )
---
+++ day5/test_day5.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pytest
+
+from day5.day5 import search_num, solve_day5_part1
+
+
+@pytest.fixture
+def day5_example1():
+    return ["FBFBBFFRLR"]
+
+
+@pytest.fixture
+def day5_example2():
+    return ["BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"]
+
+
+@pytest.fixture
+def input_day5():
+    input_path = Path(__file__).parent / "input.txt"
+    with open(input_path, "r") as input_file:
+        return [l.strip() for l in input_file.readlines() if l]
+
+
+def test_day5_part1_example(day5_example1, day5_example2):
+    assert (
+        solve_day5_part1(day5_example1, min_row=0, max_row=127, min_col=0, max_col=7)
+        == 357
+    )
+    assert (
+        solve_day5_part1(day5_example2, min_row=0, max_row=127, min_col=0, max_col=7)
+        == 820
+    )
+
+
+def test_day5_part1_input(input_day5):
+    assert (
+        solve_day5_part1(input_day5, min_row=0, max_row=127, min_col=0, max_col=7)
+        == 926
+    )
